2fe6e933674a90ae744d790c824e3d8a9f36d671
cliftondavies <clifton.davies@outlook.com>
2020-10-24 00:44:08 +0100

    Fix issue with toggling book status

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ function Book(author, title, pages, status) {
   this.status = status;
 }
 
-Book.prototype.toggleStatus = function() {
-  // (this.status === 'read') ? this.status = 'unread' : this.status = 'read';
-  console.log('toggleStatus');
-}
+// Book.prototype.toggleStatus = function() {
+//   // (this.status === 'read') ? this.status = 'unread' : this.status = 'read';
+//   console.log('toggleStatus');
+// }
 
-const newBookBtn = document.getElementById('newBookBtn');
+const bookBtn = document.getElementById('bookBtn');
 
 const formWrapper = document.getElementById('form-wrapper');
 
@@ -61,18 +61,18 @@ function showBook(book) {
   const section = document.querySelector('#bookDisplay');
 
   let bookLibrary = JSON.parse(localStorage.getItem('library'));
-  // let book = bookLibrary[bookLibrary.length - 1]; //bookLibrary will be null at the start
+  let newBook = bookLibrary.find(element => element === book); //bookLibrary will be null at the start
   // console.log(book);
 
   const card = document.createElement('div');
   const author = document.createElement('h3');
-  author.textContent = book.author;
+  author.textContent = newBook.author;
   const bookTitle = document.createElement('h2');
-  bookTitle.textContent = book.title;
+  bookTitle.textContent = newBook.title;
   const pages = document.createElement('h5');
-  pages.textContent = book.pages;
+  pages.textContent = newBook.pages;
   const status = document.createElement('button');
-  status.textContent = book.status;
+  status.textContent = newBook.status;
   const deleteBook = document.createElement('button');
   deleteBook.textContent = 'delete book';
   section.appendChild(card);
@@ -85,21 +85,10 @@ function showBook(book) {
   // status.onclick = () => { book.toggleStatus };
 
   status.onclick = () => {
-    if(book.status === 'read') {
-      let index = bookLibrary.indexOf(book);
-      book.status = 'unread';
-      bookLibrary[index] = book;
-      localStorage.setItem('library', JSON.stringify(bookLibrary));
-      status.textContent = book.status;
-      console.log(book.status);
-    } else {
-      let index = bookLibrary.indexOf(book);
-      book.status = 'read';
-      bookLibrary[index] = book;
-      localStorage.setItem('library', JSON.stringify(bookLibrary));
-      status.textContent = book.status;
-      console.log(book.status);
-    }
+    newBook.status === 'read' ? newBook.status = 'unread' : newBook.status = 'read';
+    localStorage.setItem('library', JSON.stringify(bookLibrary));
+    status.textContent = newBook.status;
+
   }
 
   deleteBook.onclick = () => {
@@ -116,43 +105,39 @@ function displayLibrary() {
 
   let bookLibrary = JSON.parse(localStorage.getItem('library'));
 
-  bookLibrary.forEach(book => { //bookLibrary will be null at the start
-    const card = document.createElement('div');
-    const author = document.createElement('h3');
-    author.textContent = book.author;
-    const bookTitle = document.createElement('h2');
-    bookTitle.textContent = book.title;
-    const pages = document.createElement('h5');
-    pages.textContent = book.pages;
-    const status = document.createElement('button');
-    status.textContent = book.status;
-    const deleteBook = document.createElement('button');
-    deleteBook.textContent = 'delete book';
-    section.appendChild(card);
-    card.appendChild(author);
-    card.appendChild(bookTitle);
-    card.appendChild(pages);
-    card.appendChild(status);
-    card.appendChild(deleteBook);
+  if (bookLibrary !== null) {
+    bookLibrary.forEach(book => {
+      const card = document.createElement('div');
+      const author = document.createElement('h3');
+      author.textContent = book.author;
+      const bookTitle = document.createElement('h2');
+      bookTitle.textContent = book.title;
+      const pages = document.createElement('h5');
+      pages.textContent = book.pages;
+      const status = document.createElement('button');
+      status.textContent = book.status;
+      const deleteBook = document.createElement('button');
+      deleteBook.textContent = 'delete book';
+      section.appendChild(card);
+      card.appendChild(author);
+      card.appendChild(bookTitle);
+      card.appendChild(pages);
+      card.appendChild(status);
+      card.appendChild(deleteBook);
 
-    // status.onclick = () => { book.toggleStatus };
+      // status.onclick = () => { book.toggleStatus };
 
-    status.onclick = () => {
-      if(book.status === 'read') {
-        book.status = 'unread';
-        localStorage.setItem('library', JSON.stringify(bookLibrary));
-        status.textContent = book.status;
-      } else {
-        book.status = 'read';
+      status.onclick = () => {
+        book.status === 'read' ? book.status = 'unread' : book.status = 'read';
         localStorage.setItem('library', JSON.stringify(bookLibrary));
         status.textContent = book.status;
       }
-    }
 
 
-    deleteBook.onclick = () => {
-      deleteBook.parentNode.remove();
-      // localStorage.removeItem(bookLibrary[bookLibrary.indexOf(book)]);
-    }
-  });
+      deleteBook.onclick = () => {
+        deleteBook.parentNode.remove();
+        // localStorage.removeItem(bookLibrary[bookLibrary.indexOf(book)]);
+      }
+    });
+  }
 }
